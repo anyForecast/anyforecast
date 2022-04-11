4655ffb5eeb7412e49a6d60eefa8cdee2cb28dcb
Ramón Amézquita <ramon.amez@kuona.co>
2022-04-11 17:03:15 -0600

    Sending ClientArgsCreator to args.py file

--- client/client/client.py
+++ client/client/client.py
@@ -1,4 +1,4 @@
-from .endpoint import EndpointCreator
+from .args import ClientArgsCreator
 from .exceptions import UnknownServiceError
 from .services import Forecast, MinioClient
 
@@ -41,46 +41,6 @@ class ClientCreator:
         return args_creator.get_client_args(
             service_name, endpoint_name, is_secure, endpoint_url,
             credentials, endpoint_bridge
-        )
-
-
-class ClientArgsCreator:
-    """Creates client args.
-
-    Since services share a common interface, a consistent procedure for
-    obtaining their constructor arguments can be achieved.
-    """
-
-    def get_client_args(self, service_name, endpoint_name, is_secure,
-                        endpoint_url, credentials, endpoint_bridge):
-
-        # Resolve endpoint data.
-        resolved = self._resolve_endpoint(
-            service_name, endpoint_name, endpoint_url, is_secure,
-            endpoint_bridge)
-
-        # Create endpoint object.
-        endpoint_config = resolved['endpoint_config']
-        endpoint = self._get_endpoint(endpoint_config)
-
-        return {'endpoint': endpoint, 'credentials': credentials}
-
-    def _get_endpoint(self, endpoint_config):
-        endpoint_creator = EndpointCreator()
-        service_name = endpoint_config['service_name']
-        endpoint = endpoint_creator.create_endpoint(
-            service_name,
-            endpoint_url=endpoint_config['endpoint_url']
-        )
-        return endpoint
-
-    def _resolve_endpoint(self, service_name, endpoint_name, endpoint_url,
-                          is_secure, endpoint_bridge):
-        return endpoint_bridge.resolve(
-            service_name=service_name,
-            endpoint_name=endpoint_name,
-            endpoint_url=endpoint_url,
-            is_secure=is_secure
         )
 
 
