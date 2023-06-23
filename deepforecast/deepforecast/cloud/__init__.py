63f03c7fd1826d014cc7992a6f66acb538d9912d
Ramón Amézquita <ramon.amez@kuona.ai>
2023-06-23 13:36:14 -0700

    Deleting deepforecast dir

--- deepforecast/deepforecast/cloud/__init__.py
+++
@@ -1,7 +0,0 @@
-from ._dataloaders import DataloaderCreator
-from ._mlflow_loader import MlFlowLoader
-
-__all__ = [
-    'MlFlowLoader',
-    'DataloaderCreator'
-]
--- deepforecast/deepforecast/inference/__init__.py
+++
@@ -1,5 +0,0 @@
-from .base_inference import BaseInference
-
-__all__ = [
-    'BaseInference'
-]
