4c51ff946b1f4759c63b9d4ed626b6e22044e420
Ramón Amézquita <ramon.amez@kuona.ai>
2023-08-06 23:08:26 -0700

    Decorating tasks

--- anyforecast/executors/__init__.py
+++ anyforecast/executors/__init__.py
@@ -1,9 +1,34 @@
+from typing import Dict
+
 from .base import Executor
-from .celery import Celery
+from .celery import CeleryExecutor
 from .local import LocalExecutor
 
+_EXECUTORS = {
+    "local": LocalExecutor,
+    "celery": CeleryExecutor
+}
+
+
+def get_executors() -> Dict[str, Executor]:
+    return _EXECUTORS
+
+
+def get_executor(name: str):
+    """Executors factory.
+
+    Parameters
+    ----------
+    name : str
+        Executor name.
+    """
+    if isinstance(name, Executor):
+        return name
+
+    return _EXECUTORS[name]()
+
+
 __all__ = [
-    'Executor',
-    'Celery',
-    'LocalExecutor'
+    'get_executors',
+    'get_executor'
 ]
---
+++ anyforecast/tasks/__init__.py
@@ -0,0 +1,5 @@
+from .examples import add
+
+__all__ = [
+    'add'
+]
